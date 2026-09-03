0969047b02c1a41f8e7cbd08f6e306ac59f1e591
Копонов Вадим <134194899+KoponovVadim@users.noreply.github.com>
2026-09-03 14:28:25 +0300

    feat: expose description variants in admin

--- partners/admin.py
+++ partners/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-from .models import ArticleSnapshot, ClientSite, DonorSite, PageTemplate, Placement, PublicationLog
+from .models import (
+    ArticleSnapshot,
+    ClientDescriptionVariant,
+    ClientSite,
+    DonorSite,
+    PageTemplate,
+    Placement,
+    PublicationLog,
+)
+
 
 class PlacementInline(admin.TabularInline):
     model = Placement
     extra = 0
+
+
+class ClientDescriptionVariantInline(admin.StackedInline):
+    model = ClientDescriptionVariant
+    extra = 1
+    fields = ("name", "text", "enabled", "position")
+
 
 @admin.register(DonorSite)
 class DonorAdmin(admin.ModelAdmin):
@@ -13,10 +29,13 @@ class DonorAdmin(admin.ModelAdmin):
     exclude = ("encrypted_password", "encrypted_api_token", "encrypted_connector_token")
     inlines = [PlacementInline]
 
+
 @admin.register(ClientSite)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ("name", "domain", "enabled")
     search_fields = ("name", "domain")
+    inlines = [ClientDescriptionVariantInline]
+
 
 admin.site.register(PageTemplate)
 admin.site.register(Placement)
