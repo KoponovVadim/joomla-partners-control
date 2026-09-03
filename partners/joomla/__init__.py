e8d47819c79a2182ce9b954c50aa043eb404dfd3
Копонов Вадим <134194899+KoponovVadim@users.noreply.github.com>
2026-09-03 11:14:27 +0300

    feat: route Joomla 3 connector auth mode

--- partners/joomla/__init__.py
+++ partners/joomla/__init__.py
@@ -1,9 +1,22 @@
 from .detector import detect_version
 
+
 def get_adapter(donor):
     from .base import JoomlaAdapter
     from .joomla3 import Joomla3Adapter
+    from .joomla3_connector import Joomla3ConnectorAdapter
     from .joomla4 import Joomla4Adapter
     from .joomla5 import Joomla5Adapter
-    adapters = {"3": Joomla3Adapter, "4": Joomla4Adapter, "5": Joomla5Adapter}
+
+    if (
+        donor.joomla_version == "3"
+        and donor.auth_mode == "connector_token"
+    ):
+        return Joomla3ConnectorAdapter(donor)
+
+    adapters = {
+        "3": Joomla3Adapter,
+        "4": Joomla4Adapter,
+        "5": Joomla5Adapter,
+    }
     return adapters.get(donor.joomla_version, JoomlaAdapter)(donor)
